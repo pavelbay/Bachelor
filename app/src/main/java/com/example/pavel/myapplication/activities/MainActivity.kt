5c47ce498e26f1294dc3b2ef76d37dbfad277592
Pavel <pavel.bayburtyan@gmail.com>
2017-10-20 13:09:29 +0200

    added libraries, permissions

---
+++ app/src/main/java/com/example/pavel/myapplication/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pavel.myapplication.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.pavel.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
