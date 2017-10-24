401e1004e2e7f58730d12b43548d9fda888853fa
Pavel <pavel.bayburtyan@gmail.com>
2017-10-24 13:46:20 +0200

    another color picker added

--- app/src/main/java/com/example/pavel/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/pavel/myapplication/activities/MainActivity.kt
@@ -1,40 +1,94 @@
 package com.example.pavel.myapplication.activities
 
+import android.app.FragmentTransaction
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Paint.Cap
 import android.graphics.Paint.Join
 import android.os.Bundle
+import android.support.annotation.ColorInt
 import android.support.v7.app.AppCompatActivity
 import com.example.pavel.myapplication.R
+import com.example.pavel.myapplication.events.ColorPickerEvents
+import com.example.pavel.myapplication.fragments.ColorPickerDialogFragment
 import com.pes.androidmaterialcolorpickerdialog.ColorPicker
 import kotlinx.android.synthetic.main.activity_main.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
 
-private const val DEFAULT_COLOR = Color.GREEN
+@ColorInt private const val DEFAULT_COLOR = Color.GREEN
+private const val COLOR_PICKER_DIALOG_TAG = "color_picker_dialog_tag"
 
 class MainActivity : AppCompatActivity() {
 
-    private var mColor = DEFAULT_COLOR
+    private val mPaint = Paint()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val paint = Paint()
-        paint.isAntiAlias = true
-        paint.isDither = true
-        paint.color = mColor
-        paint.style = Paint.Style.STROKE
-        paint.strokeJoin = Join.ROUND
-        paint.strokeCap = Cap.ROUND
-        paint.strokeWidth = 12F
+        mPaint.isAntiAlias = true
+        mPaint.isDither = true
+        mPaint.color = DEFAULT_COLOR
+        mPaint.style = Paint.Style.STROKE
+        mPaint.strokeJoin = Join.ROUND
+        mPaint.strokeCap = Cap.ROUND
+        mPaint.strokeWidth = 12F
 
-        mDrawingView.paint = paint
-        val colorPicker = ColorPicker(this, Color.alpha(mColor), Color.red(mColor), Color.green(mColor), Color.blue(mColor))
+        mDrawingView.paint = mPaint
+        val colorPicker = ColorPicker(this, Color.alpha(DEFAULT_COLOR), Color.red(DEFAULT_COLOR), Color.green(DEFAULT_COLOR), Color.blue(DEFAULT_COLOR))
         colorPicker.setCallback { color ->
-            paint.color = color
+            mPaint.color = color
             colorPicker.dismiss()
         }
-        mFloatingActionButton.setOnClickListener { colorPicker.show() }
+        mFloatingActionButton.setOnClickListener {
+            //            colorPicker.show()
+            showColorPickerDialog()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this)
+        }
+    }
+
+    private fun showColorPickerDialog() {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        removeDialogIfExists(fragmentTransaction, COLOR_PICKER_DIALOG_TAG)
+
+        val colorPickerDialog = ColorPickerDialogFragment.newInstance(R.layout.layout_color_picker, R.id.color_picker_ok_button, DEFAULT_COLOR)
+        colorPickerDialog.show(fragmentTransaction, COLOR_PICKER_DIALOG_TAG)
+    }
+
+    private fun removeDialogIfExists(fragmentTransaction: FragmentTransaction?, tag: String) {
+        val previousFragment = fragmentManager.findFragmentByTag(tag)
+        previousFragment?.let {
+            fragmentTransaction?.remove(previousFragment)
+        }
+
+        fragmentTransaction?.addToBackStack(null)
+    }
+
+    @Subscribe
+    fun onColorPickerDialogDismiss(colorPickerOkButtonEvent: ColorPickerEvents.ColorPickerOkButtonEvent) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        removeDialogIfExists(fragmentTransaction, COLOR_PICKER_DIALOG_TAG)
+        fragmentTransaction.commit()
+    }
+
+    @Subscribe
+    fun onColorSelectedEvent(colorSelectedEvent: ColorPickerEvents.ColorSelectedEvent) {
+        mPaint.color = colorSelectedEvent.color
     }
 }
---
+++ app/src/main/java/com/example/pavel/myapplication/events/ColorPickerEvents.kt
@@ -0,0 +1,8 @@
+package com.example.pavel.myapplication.events
+
+class ColorPickerEvents {
+
+    class ColorPickerOkButtonEvent
+
+    class ColorSelectedEvent(val color: Int)
+}
