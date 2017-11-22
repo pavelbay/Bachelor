26e6c3ea7d7aef8f03eff6547234588198b5bb87
Pavel <pavel.bayburtyan@gmail.com>
2017-11-22 11:19:55 +0100

    use fragment pager adapter

--- app/src/main/java/com/pavel/augmented/presentation/pageradapter/MainPagerAdapter.kt
+++ app/src/main/java/com/pavel/augmented/presentation/pageradapter/MainPagerAdapter.kt
@@ -3,16 +3,12 @@ package com.pavel.augmented.presentation.pageradapter
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.app.FragmentStatePagerAdapter
 import com.pavel.augmented.presentation.canvas.CanvasFragment
 import com.pavel.augmented.presentation.galerie.GalerieFragment
 import com.pavel.augmented.presentation.map.MapFragment
-import android.os.Parcelable
 
 
-
-
-class MainPagerAdapter(fragmentManager: FragmentManager, private val names: Array<String>) : FragmentStatePagerAdapter(fragmentManager) {
+class MainPagerAdapter(fragmentManager: FragmentManager, private val names: Array<String>) : FragmentPagerAdapter(fragmentManager) {
 
     override fun getCount(): Int = names.size
 
