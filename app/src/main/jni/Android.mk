a432f5ea984e7bf723d5b7184a24509fb74941a8
Pavel <pavel.bayburtyan@gmail.com>
2017-12-15 15:01:02 +0100

    changed android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,8 +1,9 @@
+LOCAL_PATH_TOP := $(call my-dir)
 include $(CLEAR_VARS)
 # make sure this path is available for libEasyAR.so
-LOCAL_PATH := $(LOCAL_PATH_TOP)/jniLibs/armeabi-v7a
+LOCAL_PATH := $(LOCAL_PATH_TOP)/../jniLibs/armeabi-v7a
 LOCAL_MODULE := EasyAR
-LOCAL_SRC_FILES := C:\Users\Pavel\Documents\Studium\Semester7\Bachelor\app\src\main\jniLibs\armeabi-v7a\libEasyAR.so
+LOCAL_SRC_FILES := libEasyAR.so
 include $(PREBUILT_SHARED_LIBRARY)
 LOCAL_LDLIBS += -lGLESv2
 LOCAL_SHARED_LIBRARIES += EasyAR
